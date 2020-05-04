1ef7c3b2f8572f006c963999d8e8e8e70bdc13c7
johanncampos <jscampos@miners.utep.edu>
2020-05-03 23:44:59 -0600

    added squid

--- project/shapes.c
+++ project/shapes.c
@@ -6,6 +6,7 @@
 void drawX(int x, int y, int size, int color) {
   u_char j;
   u_char len = size/2;
+
   for (j = 0; j < len; j++) {
     drawPixel(x-j, y-j, color);
     drawPixel(x-j, y+j, color);
@@ -17,64 +18,107 @@ void drawX(int x, int y, int size, int color) {
 void drawDiamond(int x, int y, int size, int color) {
   u_char j;
   u_char len = size/2;
-  int i;
-  /*
+  u_char i;
+
+  for(j = 0; j < len+1; j++){
+    for(i = 0; i < size; i++){
+      if(i >= len-j && i <= len+j){
+	drawPixel(x-len+i,y-len+j,color);
+      }
+    }
+  }
+  for(j =len+1; j<size; j++){
+    for(i = 0; i < size; i++) {
+      if(i >= j-len && i <= size-(j-len)){
+	drawPixel(x-len+i,y-len+j,color);
+      }
+    }
+  }
+}
+
+void drawDiamondOutline(int x, int y, int size, int color) {
+  u_char j;
+  u_char len = size/2;
+  
   for (j = 0; j < len; j++) {
     drawPixel(x-j, y-(len-j), COLOR_YELLOW);
     drawPixel(x-j, y+(len-j), COLOR_BLUE);
     drawPixel(x-j+len, y-(len-j)+len, COLOR_RED);
     drawPixel(x-j+len, y+(len-j)-len, COLOR_GREEN);
   }
-  */
-  for(j=0;j<len+1;j++){
-    for(i = 0; i < size; i++){
-      if(i>=len-j && i<=len+j){
-	drawPixel(x-len+i,y-len+j,color);
-      }
-    }
-  }
-  for(j=len+1;j<size;j++){
-    for(i = 0; i < size; i++) {
-      if(i>=j-len && i<=size-(j-len)){
-	drawPixel(x-len+i,y-len+j,color);
-      }
-    }
-  }
 }
 
 void drawSquare(int x, int y, int size, int color) {
   u_char j;
   u_char len = size/2;
-  /*
-  for (j = 0; j < size; j++) {
-    drawPixel(x-len+j, y-len, COLOR_YELLOW);
-    drawPixel(x-len, y-len+j, COLOR_BLUE);
-    drawPixel(x-len+j, y+len, COLOR_RED);
-    drawPixel(x+len, y-len+j, COLOR_GREEN);
-  }
-  */
-  for(j=0;j<size;j++){
+
+  for(j = 0; j < size; j++){
     for(int i = 0; i<size; i++) {
       drawPixel(x-len+i, y-len+j, color);
     }
   }
 }
 
+void drawSquareOutline(int x, int y, int size, int color) {
+  u_char j;
+  u_char len = size/2;
+
+  for (j = 0; j < size; j++) {
+    drawPixel(x-len+j, y-len, COLOR_YELLOW);
+    drawPixel(x-len, y-len+j, COLOR_BLUE);
+    drawPixel(x-len+j, y+len, COLOR_RED);
+    drawPixel(x+len, y-len+j, COLOR_GREEN);
+  }
+}
+
 void drawTriangle(int x, int y, int size, int color) {
   u_char j;
   u_char len = size/2;
-  /*
+
+  for(j = 0; j < size; j++){
+    for(int i = 0; i < size; i++) {
+      if(i>=len-(j/2) && i<=len+(j/2)) {
+	drawPixel(x-len+i, y-len+j, color);
+      }
+    }
+  }
+}
+
+void drawTriangleOutline(int x, int y, int size, int color) {
+  u_char j;
+  u_char len = size/2;
+  
   for (j = 0; j < size; j++) {
     drawPixel(x-(j/2), y-(len-j), COLOR_YELLOW);
     drawPixel(x, y-len+j, COLOR_BLUE);
     drawPixel(x-len+j, y+len, COLOR_RED);
     drawPixel(x+(j/2), y-(len-j), COLOR_GREEN);
   }
-  */
-  for(j=0;j<size;j++){
-    for(int i = 0; i < size; i++) {
-      if(i>=len-(j/2) && i<=len+(j/2)) {
-	drawPixel(x-len+i, y-len+j, color);
+}
+
+void drawSquid(int x, int y, int size, int color) {
+  u_char i, j;
+  u_char len = size/2;
+  
+  //Head (/\)
+  for (j = 3; j < size/3; j++){
+    for (i = 0; i < size; i++){
+      if (i >= len-j && i <= len+j){
+	drawPixel(x-len+i,y-len+j,color);
+      }
+    }
+  }
+  //Body (||)
+  for (j = size/3; j < 2*size/3; j++){
+    for (i = 3*len/5; i <= 7*len/5; i++) {
+      drawPixel(x-len+i, y-len+j, color);
+    }
+  }
+  //Tentacles (,,)
+  for (j = 2*size/3; j < size-3; j++) {
+    for (i = len/2; i < 3*len/2; i++) {
+      if (i % 3 == 0) {
+	drawPixel(x+i-len+1, y-len+j-1, color);
       }
     }
   }
--- project/shapes.h
+++ project/shapes.h
@@ -1,12 +1,18 @@
-#ifndef x_included
-#define x_included
+#ifndef shapes_included
+#define shapes_included
 
 void drawX(int x, int y, int size, int color);
 
 void drawDiamond(int x, int y, int size, int color);
 
+void drawDiamondOutline(int x, int y, int size, int color);
+
 void drawSquare(int x, int y, int size, int color);
 
-void drawTriangle(int x, int y, int size, int color);
+void drawSquareOutline(int x, int y, int size, int color);
+
+void drawTriangleOutline(int x, int y, int size, int color);
+
+void drawSquid(int x, int y, int size, int color);
 
 #endif
